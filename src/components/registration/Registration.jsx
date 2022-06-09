d21940bf1dab3a9011b4695304c3cdc9d0162b84
amtenu <amannov21@gmail.com>
2022-06-08 18:18:28 -0600

    Created a Register component

---
+++ src/components/registration/Registration.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutline'
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Checkbox from "@mui/material/Checkbox";
+const Signup = () => {
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  const headerStyle = { margin: 0 };
+  const avatarStyle = { backgroundColor: "#d64224" };
+  const marginTop = { marginTop: 5 };
+  const btnstyle={backgroundColor:"#d64224"}
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <AddCircleOutlineOutlinedIcon />
+          </Avatar>
+          <h2 style={headerStyle}>Register</h2>
+          <Typography variant="caption" gutterBottom>
+            Please fill this form to create an account !
+          </Typography>
+        </Grid>
+        <form>
+          <TextField fullWidth label="Name" placeholder="Enter your name" />
+          <TextField fullWidth label="Email" placeholder="Enter your email" />
+          <FormControl component="fieldset" style={marginTop}>
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="gender"
+              style={{ display: "initial" }}
+            >
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+            </RadioGroup>
+          </FormControl>
+          <TextField
+            fullWidth
+            label="Phone Number"
+            placeholder="Enter your phone number"
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            placeholder="Enter your password"
+          />
+          <TextField
+            fullWidth
+            label="Confirm Password"
+            placeholder="Confirm your password"
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedA" />}
+            label="I accept the terms and conditions."
+          />
+          <Button type="submit" variant="contained" color="primary" style={btnstyle} >
+           Register
+          </Button>
+        </form>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default Signup;
