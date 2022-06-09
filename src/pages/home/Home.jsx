51b1782074d8c0cc226346d7c1c48bf8f0d9c661
amtenu <amannov21@gmail.com>
2022-06-08 19:38:52 -0600

    Removed Header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/header/Header";
+
 import NavBar from "../../components/navbar/NavBar";
 import "./home.css";
 
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <NavBar />
-      <Header />
+      
     </div>
   );
 };
