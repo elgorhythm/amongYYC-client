6fbc7778028ef158421c325e22eaadc2ab4985ca
amtenu <amannov21@gmail.com>
2022-06-09 18:55:53 -0600

    Added siginin,signup and leaderBoard pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import Home from "./pages/home/Home";
+import LeaderBoard from "./pages/home/LeaderBoard";
+import SignIn from "./pages/home/SignIn";
+import SignUp from "./pages/home/SignUp";
+
 
 
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-     <Route path="/" element={<Home/>}/>
-   </Routes>
-   </BrowserRouter> 
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/leaderBoard" element={<LeaderBoard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
