ffe36a6ce3f436cd9a36b7ee43e8d7c39f1e0828
amtenu <amannov21@gmail.com>
2022-06-13 01:32:04 -0600

    Added AppBarMenus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/appBarMenus/About";
+import Dashboard from "./components/component/Dashboard";
+import Logout from "./components/component/Logout";
+import Profile from "./components/component/Profile";
+import Events from "./components/appBarMenus/Events"
 
 
 import Home from "./pages/home/Home";
-import LeaderBoard from "./pages/home/LeaderBoard";
+
 import SignIn from "./pages/home/SignIn";
 import SignUp from "./pages/home/SignUp";
+import Game from "./components/appBarMenus/Game";
+import Sponsors from "./components/appBarMenus/Sponsors";
+import Account from "./components/component/Account";
 
 
 
@@ -16,7 +24,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/leaderBoard" element={<LeaderBoard />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Logout" element={<Logout />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path='About' element={<About/>}/>
+        <Route path='Events' element={<Events/>}/>
+        <Route path='Game' element={<Game/>}/>
+        <Route path='Sponsors' element={<Sponsors/>}/>
+        <Route path='Account' element={<Account/>}/>
       </Routes>
     </BrowserRouter>
   );
