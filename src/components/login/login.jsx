6b8a2b9dc388b5deb00ce9e54fa67ce0d8cc01b0
AdnanAther <adnan.ather@live.ca>
2022-06-13 12:24:34 -0600

    user can sign-in succcessfully

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,45 +1,106 @@
-import React from 'react'
-import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@mui/material'
-import LockIcon from '@mui/icons-material/Lock';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-const Login=()=>{
+import React, { useContext, useState } from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#d64224'}
-    const btnstyle={margin:'8px 0',backgroundColor:"#d64224"}
-    return(
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockIcon/></Avatar>
-                    <h2>Sign In</h2>
-                </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
-                <FormControlLabel
-                    control={
-                    <Checkbox
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label="Remember me"
-                 />
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography >
-                     <Link href="#" >
-                        Forgot password ?
-                </Link>
-                </Typography>
-                <Typography > Do you have an account ?
-                     <Link href="#" >
-                        Sign Up 
-                </Link>
-                </Typography>
-            </Paper>
+const Login = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#d64224" };
+  const btnstyle = { margin: "8px 0", backgroundColor: "#d64224" };
+
+  const navigate=useNavigate()
+  const authContext = useContext(AuthContext);
+  const loginFn = authContext.login;
+  const loginError = authContext.authError;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  const signin = () => {
+    if (!email || !password) {
+      setError("All fields are required");
+    } else {
+      try {
+        loginFn(email, password);
+        const loginError = authContext.authError;
+        loginError? setError(loginError) : navigate("/");
+      } catch {
+        setError(loginError.toString().slice(9));
+      }
+    }
+  };
+
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockIcon />
+          </Avatar>
+          <h2>Sign In</h2>
         </Grid>
-    )
-}
+        <TextField
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+         <p style={{ color: "red" }}>{error}</p>
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+          onClick={signin}
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          {" "}
+          Do you have an account ?<Link href="#">Sign Up</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/home/SignIn.jsx
+++ src/pages/home/SignIn.jsx
@@ -1,21 +1,27 @@
-import { Box, Stack } from '@mui/material';
-import React from 'react'
-import NavBar from "../../components/navbar/NavBar"
-import RightBar from '../../components/rightBar/RightBar';
-import SideBar from '../../components/sideBar/SideBar';
-import Login from "../../components/login/login"
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+import RightBar from "../../components/rightBar/RightBar";
+import SideBar from "../../components/sideBar/SideBar";
+import Login from "../../components/login/login";
+import FirebaseProvider from "../../providers/FirebaseProvider";
+import AuthProvider from "../../providers/AuthProvider";
 
 function SignIn() {
-    return (
-        <Box>
-          <NavBar />
-          <Stack direction="row" spacing={2} justifyContent="space-between">
-            <SideBar />
-            <Login/>
-            <RightBar />
-          </Stack>
-        </Box>
-      );
+  return (
+    <Box>
+      <NavBar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <SideBar />
+        <FirebaseProvider>
+          <AuthProvider>
+            <Login />
+          </AuthProvider>
+        </FirebaseProvider>
+        <RightBar />
+      </Stack>
+    </Box>
+  );
 }
 
-export default SignIn
+export default SignIn;
