0c4a79786cfd88c75b6dc8c13fcfffa49ed1e787
amtenu <amannov21@gmail.com>
2022-06-18 15:01:21 -0600

    Added UsersList component to AllUsersList page

---
+++ src/pages/home/AllUsersList.jsx
@@ -0,0 +1,29 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+
+import SideBar from "../../components/sideBar/SideBar";
+import NavBar from "../../components/navbar/NavBar";
+import RightBar from "../../components/rightBar/RightBar";
+import FirebaseProvider from "../../providers/FirebaseProvider";
+import AuthProvider from "../../providers/AuthProvider";
+import Profile from "../../components/component/Profile"
+import UsersList from "../../components/component/UsersList";
+const AllUsersList
+ = () => {
+  return (
+    <Box>
+      <FirebaseProvider>
+        <AuthProvider>
+          <NavBar />
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <SideBar />
+            <UsersList />
+            <RightBar />
+          </Stack>
+        </AuthProvider>
+      </FirebaseProvider>
+    </Box>
+  );
+};
+
+export default  AllUsersList;
