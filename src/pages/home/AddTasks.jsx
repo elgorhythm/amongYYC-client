69875848fe5c4b34632b0b08a9c5649f6be7e557
amtenu <amannov21@gmail.com>
2022-06-21 18:34:29 -0600

    add task component

---
+++ src/pages/home/AddTasks.jsx
@@ -0,0 +1,28 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+
+import SideBar from "../../components/sideBar/SideBar";
+import NavBar from "../../components/navbar/NavBar";
+import RightBar from "../../components/rightBar/RightBar";
+import FirebaseProvider from "../../providers/FirebaseProvider";
+import AuthProvider from "../../providers/AuthProvider";
+
+import AddTask from "../../components/component/AddTask";
+const AddTasks = () => {
+  return (
+    <Box>
+      <FirebaseProvider>
+        <AuthProvider>
+          <NavBar />
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <SideBar />
+            <AddTask />
+            <RightBar />
+          </Stack>
+        </AuthProvider>
+      </FirebaseProvider>
+    </Box>
+  );
+};
+
+export default AddTasks;
