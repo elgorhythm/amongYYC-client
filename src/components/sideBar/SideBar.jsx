1df9e7f1a496b0a77a6aee114ec546f2c3420561
amtenu <amannov21@gmail.com>
2022-07-24 20:04:40 -0600

    Added Side bar and it's scss

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,10 +1,104 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import React from "react";
+import "./sideBar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import TaskIcon from "@mui/icons-material/Task";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AddReactionIcon from "@mui/icons-material/AddReaction";
+import { Link } from "react-router-dom";
 
-function SideBar() {
+import { useNavigate } from "react-router-dom";
+
+
+
+const SideBar = () => {
+ 
+
+  const navigate = useNavigate();
+
+  const signOut = () => {
+   
+    navigate("/login");
+  };
+
   return (
-    <Box >Sidebar</Box>
-  ) 
-}
+    <div className="Sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          {/* <span className="logo">Among yyc</span> */}
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>DashBoard</span>
+          </li>
+          <p className="title">LIST</p>
+          <Link to="users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="tasks" style={{ textDecoration: "none" }}>
+            <li>
+              <TaskIcon className="icon" />
+              <span>Tasks</span>
+            </li>
+          </Link>
+          <li>
+            <AddReactionIcon className="icon" />
+            <span>Tricksters</span>
+          </li>
+          <li>
+            <EmojiEventsIcon className="icon" />
+            <span>Raffles</span>
+          </li>
+          <p className="title">GAME INFO</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span onClick={signOut}>Log out</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+         
+        ></div>
+        <div
+          className="colorOption"
+          
+        ></div>
+      </div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
