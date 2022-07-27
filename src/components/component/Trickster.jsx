f08b17d15b330e67f782b4cbf8a06bb2f38b37ad
amtenu <amannov21@gmail.com>
2022-07-26 19:25:01 -0600

    Fix Trickster now can be a choice , select times from drop down and choose who the correct imposter

--- src/components/component/Trickster.jsx
+++ src/components/component/Trickster.jsx
@@ -2,51 +2,70 @@ import { collection, doc, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { useContext } from "react";
 import { FirebaseContext } from "../../providers/FirebaseProvider";
-import { Box } from "@mui/material";
+import { Box, Button, MenuItem, Select, Typography } from "@mui/material";
+
 const Trickster = () => {
+  
   const fbContext = useContext(FirebaseContext);
   const db = fbContext.db;
 
-  let timesActive = [
-    "08:00",
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-  ];
-  const [imposterId, setImposterId] = useState("");
-  const [imposterName, setImposterName] = useState("");
-  const [imposterDescription, setImposterDescription] = useState("");
-  const [imposterHint, setImposterHint] = useState("");
-  const [imposterImage, setImposterImage] = useState("");
-  const [imposterActive, setImposterActive] = useState(true);
-  const [imposterActiveTimes, setImposterActiveTimes] = useState(timesActive);
+  
 
   const addImposter = async () => {
     try {
-      let imposterCollectionRef = doc(db, "imposters", imposterId);
-      await setDoc(imposterCollectionRef, {
-        imposterId: imposterId,
-        name: imposterName,
-        description: imposterDescription,
-        hint: imposterHint,
-        image: imposterImage,
-        active: imposterActive,
-        activeTimes: imposterActiveTimes,
-      });
-      setImposterName("");
-      setImposterDescription("");
-      setImposterHint("");
-      setImposterImage("");
-      setImposterActive(true);
-      setImposterActiveTimes(timesActive);
+      const imposterCollectionRef = collection(db, "test");
+      let imposterDocRef = doc(imposterCollectionRef, "imposter1");
+      let imposter = {
+        Imposters: [
+          { name: name1, uri: img1 },
+          { name: name2, uri: img2 },
+          { name: name3, uri: img3 },
+          { name: name4, uri: img4 },
+        ],
+        activeTimes: [
+          activeHr1 + ":" + activeMin1,
+          activeHr2 + ":" + activeMin2,
+          activeHr3 + ":" + activeMin3,
+          activeHr4 + ":" + activeMin4,
+          activeHr5 + ":" + activeMin5,
+        ],
+        hint: hint,
+        correctAnswer: answer,
+      };
+
+      await setDoc(imposterDocRef, imposter);
+      console.log("Imposter added successfully!");
+      
+      
     } catch (ex) {
       console.log("Firestore Add failure", ex.message);
     }
   };
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [name3, setName3] = useState("");
+  const [name4, setName4] = useState("");
+
+  const [img1, setImg1] = useState("");
+  const [img2, setImg2] = useState("");
+  const [img3, setImg3] = useState("");
+  const [img4, setImg4] = useState("");
+
+  const [answer, setAnswer] = useState("");
+
+  const [hint, setHint] = useState("");
+
+  const [activeHr1, setActiveHr1] = useState();
+  const [activeHr2, setActiveHr2] = useState();
+  const [activeHr3, setActiveHr3] = useState();
+  const [activeHr4, setActiveHr4] = useState();
+  const [activeHr5, setActiveHr5] = useState();
+
+  const [activeMin1, setActiveMin1] = useState();
+  const [activeMin2, setActiveMin2] = useState();
+  const [activeMin3, setActiveMin3] = useState();
+  const [activeMin4, setActiveMin4] = useState();
+  const [activeMin5, setActiveMin5] = useState();
 
   return (
     <Box>
@@ -57,132 +76,375 @@ const Trickster = () => {
           flexDirection: "column",
         }}
       >
-        <label htmlFor="imposterId">Trickster Id</label>
-        <input
+        <Typography> Add personalities for the trickster function</Typography>
+        <div
           style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
-          
+            display: "flex",
+            flexDirection: "row",
           }}
-          name={imposterId}
-          value={imposterId}
-          onChange={(e) => setImposterId(e.target.value)}
-        />
-        <label htmlFor="imposterName">Trickster Name</label>
-        <input
+        >
+          <label>Trickster 1</label>
+          <input
+            type="text"
+            value={name1}
+            onChange={(e) => setName1(e.target.value)}
+          />
+          <input
+            type="text"
+            value={img1}
+            onChange={(e) => setImg1(e.target.value)}
+          />
+        </div>
+        <div
           style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
+            display: "flex",
+            flexDirection: "row",
           }}
-          name={imposterName}
-          value={imposterName}
-          onChange={(e) => setImposterName(e.target.value)}
-        />
-        <label htmlFor="imposterDescription">Trickster Description</label>
-        <input
+        >
+          <label>Trickster 2</label>
+          <input
+            type="text"
+            value={name2}
+            onChange={(e) => setName2(e.target.value)}
+          />
+          <input
+            type="text"
+            value={img2}
+            onChange={(e) => setImg2(e.target.value)}
+          />
+        </div>
+        <div
           style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
+            display: "flex",
+            flexDirection: "row",
           }}
-          name={imposterDescription}
-          value={imposterDescription}
-          onChange={(e) => setImposterDescription(e.target.value)}
-        />
-        <label htmlFor="imposterHint">Trickster Hint</label>
-        <input
+        >
+          <label>Trickster 3</label>
+          <input
+            type="text"
+            value={name3}
+            onChange={(e) => setName3(e.target.value)}
+          />
+          <input
+            type="text"
+            value={img3}
+            onChange={(e) => setImg3(e.target.value)}
+          />
+        </div>
+        <div
           style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
+            display: "flex",
+            flexDirection: "row",
           }}
-          name={imposterHint}
-          value={imposterHint}
-          onChange={(e) => setImposterHint(e.target.value)}
-        />
-        <label htmlFor="imposterImage">Trickster Image</label>
+        >
+          <label>Trickster 4</label>
+          <input
+            type="text"
+            value={name4}
+            onChange={(e) => setName4(e.target.value)}
+          />
+          <input
+            type="text"
+            value={img4}
+            onChange={(e) => setImg4(e.target.value)}
+          />
+        </div>
+
+        <label> Active times</label>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Select
+            label="Hours"
+            labelId="Hours"
+            value={activeHr1}
+            onChange={(e) => setActiveHr1(e.target.value)}
+          >
+            <MenuItem value={"09"}>09</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+            <MenuItem value={"13"}>13</MenuItem>
+            <MenuItem value={"14"}>14</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"16"}>16</MenuItem>
+            <MenuItem value={"17"}>17</MenuItem>
+            <MenuItem value={"18"}>18</MenuItem>
+            <MenuItem value={"19"}>19</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+          </Select>
+          <Select
+            label="Minutes"
+            labelId="Minutes"
+            value={activeMin1}
+            onChange={(e) => setActiveMin1(e.target.value)}
+          >
+            <MenuItem value={"00"}>00</MenuItem>
+            <MenuItem value={"05"}>05</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+            <MenuItem value={"25"}>25</MenuItem>
+            <MenuItem value={"30"}>30</MenuItem>
+            <MenuItem value={"35"}>35</MenuItem>
+            <MenuItem value={"40"}>40</MenuItem>
+            <MenuItem value={"45"}>45</MenuItem>
+            <MenuItem value={"50"}>50</MenuItem>
+            <MenuItem value={"55"}>55</MenuItem>
+          </Select>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Select
+            label="Hours"
+            labelId="Hours"
+            value={activeHr2}
+            onChange={(e) => setActiveHr2(e.target.value)}
+          >
+            <MenuItem value={"09"}>09</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+            <MenuItem value={"13"}>13</MenuItem>
+            <MenuItem value={"14"}>14</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"16"}>16</MenuItem>
+            <MenuItem value={"17"}>17</MenuItem>
+            <MenuItem value={"18"}>18</MenuItem>
+            <MenuItem value={"19"}>19</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+          </Select>
+          <Select
+            label="Minutes"
+            labelId="Minutes"
+            value={activeMin2}
+            onChange={(e) => setActiveMin2(e.target.value)}
+          >
+            <MenuItem value={"00"}>00</MenuItem>
+            <MenuItem value={"05"}>05</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+            <MenuItem value={"25"}>25</MenuItem>
+            <MenuItem value={"30"}>30</MenuItem>
+            <MenuItem value={"35"}>35</MenuItem>
+            <MenuItem value={"40"}>40</MenuItem>
+            <MenuItem value={"45"}>45</MenuItem>
+            <MenuItem value={"50"}>50</MenuItem>
+            <MenuItem value={"55"}>55</MenuItem>
+          </Select>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Select
+            label="Hours"
+            labelId="Hours"
+            value={activeHr3}
+            onChange={(e) => setActiveHr3(e.target.value)}
+          >
+            <MenuItem value={"09"}>09</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+            <MenuItem value={"13"}>13</MenuItem>
+            <MenuItem value={"14"}>14</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"16"}>16</MenuItem>
+            <MenuItem value={"17"}>17</MenuItem>
+            <MenuItem value={"18"}>18</MenuItem>
+            <MenuItem value={"19"}>19</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+          </Select>
+          <Select
+            label="Minutes"
+            labelId="Minutes"
+            value={activeMin3}
+            onChange={(e) => setActiveMin3(e.target.value)}
+          >
+            <MenuItem value={"00"}>00</MenuItem>
+            <MenuItem value={"05"}>05</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+            <MenuItem value={"25"}>25</MenuItem>
+            <MenuItem value={"30"}>30</MenuItem>
+            <MenuItem value={"35"}>35</MenuItem>
+            <MenuItem value={"40"}>40</MenuItem>
+            <MenuItem value={"45"}>45</MenuItem>
+            <MenuItem value={"50"}>50</MenuItem>
+            <MenuItem value={"55"}>55</MenuItem>
+          </Select>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Select
+            label="Hours"
+            labelId="Hours"
+            value={activeHr4}
+            onChange={(e) => setActiveHr4(e.target.value)}
+          >
+            <MenuItem value={"09"}>09</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+            <MenuItem value={"13"}>13</MenuItem>
+            <MenuItem value={"14"}>14</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"16"}>16</MenuItem>
+            <MenuItem value={"17"}>17</MenuItem>
+            <MenuItem value={"18"}>18</MenuItem>
+            <MenuItem value={"19"}>19</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+          </Select>
+          <Select
+            label="Minutes"
+            labelId="Minutes"
+            value={activeMin4}
+            onChange={(e) => setActiveMin4(e.target.value)}
+          >
+            <MenuItem value={"00"}>00</MenuItem>
+            <MenuItem value={"05"}>05</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+            <MenuItem value={"25"}>25</MenuItem>
+            <MenuItem value={"30"}>30</MenuItem>
+            <MenuItem value={"35"}>35</MenuItem>
+            <MenuItem value={"40"}>40</MenuItem>
+            <MenuItem value={"45"}>45</MenuItem>
+            <MenuItem value={"50"}>50</MenuItem>
+            <MenuItem value={"55"}>55</MenuItem>
+          </Select>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <Select
+            label="Hours"
+            labelId="Hours"
+            value={activeHr5}
+            onChange={(e) => setActiveHr5(e.target.value)}
+          >
+            <MenuItem value={"09"}>09</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+            <MenuItem value={"13"}>13</MenuItem>
+            <MenuItem value={"14"}>14</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"16"}>16</MenuItem>
+            <MenuItem value={"17"}>17</MenuItem>
+            <MenuItem value={"18"}>18</MenuItem>
+            <MenuItem value={"19"}>19</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+          </Select>
+          <Select
+            label="Minutes"
+            labelId="Minutes"
+            value={activeMin5}
+            onChange={(e) => setActiveMin5(e.target.value)}
+          >
+            <MenuItem value={"00"}>00</MenuItem>
+            <MenuItem value={"05"}>05</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"15"}>15</MenuItem>
+            <MenuItem value={"20"}>20</MenuItem>
+            <MenuItem value={"25"}>25</MenuItem>
+            <MenuItem value={"30"}>30</MenuItem>
+            <MenuItem value={"35"}>35</MenuItem>
+            <MenuItem value={"40"}>40</MenuItem>
+            <MenuItem value={"45"}>45</MenuItem>
+            <MenuItem value={"50"}>50</MenuItem>
+            <MenuItem value={"55"}>55</MenuItem>
+          </Select>
+        </div>
+
         <input
-          style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
-          }}
-          name={imposterImage}
-          value={imposterImage}
-          onChange={(e) => setImposterImage(e.target.value)}
+          type="text"
+          placeholder="Trickster Hint"
+          value={hint}
+          onChange={(e) => setHint(e.target.value)}
         />
-        <label htmlFor="imposterActive">Trickster Active</label>
-        <input
-          style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
-          }}
-          name={imposterActive}
-          value={imposterActive}
-          onChange={(e) => setImposterActive(e.target.value)}
-        />
-        <label htmlFor="imposterActiveTimes">Trickster Active Times</label>
-        <input
-          style={{
-            width: "100%",
-            marginBottom: 10,
-            marginTop: 10,
-            padding: 10,
-            borderRadius: 5,
-          }}
-          name={imposterActiveTimes}
-          value={imposterActiveTimes}
-          onChange={(e) => setImposterActiveTimes(e.target.value)}
-        />
+        <Typography
+              
+            >
+              {" "}
+             Select the correct answer for the hint
+            </Typography>
+            <div class="form-check">
+              <input
+                value={name1}
+                onChange={(e) => {
+                  setAnswer(e.target.value);
+                }}
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+              />
+              <label class="form-check-label" for="flexRadioDefault1">
+                {" "}
+               Trickster 1 {" "}
+              </label>
+            </div>
+            <div class="form-check">
+              <input 
+                value={name2}
+                onChange={(e) => {
+                  setAnswer(e.target.value);
+                }
+                }
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+              />
+              <label class="form-check-label" for="flexRadioDefault2">
+                {" "}
+                Trickster 2 {" "}
+              </label>
+            </div>
+            <div class="form-check">
+              <input 
+                value={name3}
+                onChange={(e) => {
+                  setAnswer(e.target.value);
+                }
+                }
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+              />
+              <label class="form-check-label" for="flexRadioDefault2">
+                {" "}
+                Trickster 3 {" "}
+              </label>
+            </div>
+            <div class="form-check">
+              <input 
+                value={name4}
+                onChange={(e) => {
+                  setAnswer(e.target.value);
+                }
+                }
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+              />
+              <label class="form-check-label" for="flexRadioDefault2">
+                {" "}
+                Trickster 4 {" "}
+              </label>
+            </div>
+            
+           
+
+
+
+
+
+
+
+
+
+      
+        <Button onClick={addImposter}>Submit</Button>
       </div>
-      <button
-        style={{
-          marginTop: 10,
-          padding: 10,
-          borderRadius: 5,
-          backgroundColor: "#00bcd4",
-          color: "white",
-        }}
-        onClick={addImposter}
-      >
-        Add
-      </button>
-      <button
-        style={{
-          marginTop: 10,
-          padding: 10,
-          borderRadius: 5,
-          backgroundColor: "#00bcd4",
-          color: "white",
-        }}
-      >
-        Edit
-      </button>
-      <button
-        style={{
-          marginTop: 10,
-          padding: 10,
-          borderRadius: 5,
-          backgroundColor: "Red",
-        }}
-      >
-        Delete
-      </button>
     </Box>
   );
 };
