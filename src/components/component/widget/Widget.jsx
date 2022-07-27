607a239d58181258fdf53ef8200cadfe476601c8
amtenu <amannov21@gmail.com>
2022-07-27 06:20:46 -0600

    Widget Added to page

---
+++ src/components/component/widget/Widget.jsx
@@ -0,0 +1,111 @@
+import "./widget.scss";
+import React from "react";
+
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+import TollIcon from "@mui/icons-material/Toll";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary data
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "Users",
+        value: "82",
+        diff: "+2",
+        isMoney: false,
+        icon: (
+          <PersonOutlinedIcon
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+        link: "See all users",
+      };
+      break;
+
+    case "tasks":
+      data = {
+        title: "Tasks",
+        value: "4",
+        diff: "4",
+        isMoney: false,
+        icon: (
+          <AddTaskIcon
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+        link: "See all tasks",
+      };
+      break;
+
+    case "coins":
+      data = {
+        title: "Coins",
+        value: "4243",
+        diff: "+2",
+        isMoney: false,
+        icon: (
+          <TollIcon
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+        link: "See all coins",
+      };
+      break;
+
+    case "donated":
+      data = {
+        title: "Donated",
+        value: "2642",
+        diff: "+25",
+        isMoney: false,
+        icon: (
+          <VolunteerActivismIcon
+          
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+
+        link: "See all donated",
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">{data.value}</span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage">
+          <KeyboardArrowUpIcon />
+          {data.diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
